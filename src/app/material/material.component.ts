1f5c288f41c3edad96e41f49d19db6ec36a219a1
Tainara Cristina Antunes de Melo <tainara_ca_melo@estudante.sesisenai.org.br>
2024-08-07 16:45:31 -0300

    main

--- src/app/material/material.component.ts
+++ src/app/material/material.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MaterialService } from '../services/material.service';
 
 
 @Component({
@@ -7,18 +9,99 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   templateUrl: './material.component.html',
   styleUrl: './material.component.scss'
 })
+
 export class MaterialComponent {
 
-}
-
-
-formulario:materiais = new FormGroup ({
-  id: new FormControl(null),
-  nome: new FormControl('',Validators.required),
-  sobrenome: new FormControl('',Validators.required),
-  telefone: new FormControl('',Validators.required),
-  email: new FormControl('',Validators.required),
-  endereco: new FormControl('',Validators.required),
-  login: new FormControl('',Validators.required),
-  })
+  constructor(
+    private materialService:MaterialService,
+    private snackbar:MatSnackBar
+  ) {this.buscaMaterial()}
   
+  
+    material:FormGroup = new FormGroup ({
+      id: new FormControl(null),
+      nome: new FormControl('',Validators.required),
+      valor: new FormControl('',Validators.required),
+      fornecedor: new FormControl('',Validators.required),
+      tipo: new FormControl('',Validators.required),
+      
+    
+ } )
+ 
+
+//Métodos dos controles do formulário
+onIncluir(){
+  this.material.reset();
+  this.material.enable();
+  }
+  
+  onSalvar(){
+    //Guarda as informações em variável para melhorar o acesso
+  let info =this.material.value;
+  //Verifica se está inserindo ou alterando com base do valor 
+  //do ID (se for null, está inserindo, se não está alterando)
+   if(info.id == null){
+  
+    this.materialService.addMaterial(info).subscribe({
+      next:(resposta)=>{
+        console.log(resposta);
+         this.snackbar.open(
+          "Material adicionado com sucesso!",
+          "Ok",
+          {
+            verticalPosition:'top',
+            horizontalPosition:'end',
+            duration:3000
+          }
+         )
+         this.onCancelar()
+      },
+      error:(erro)=>{
+        console.log(erro);
+        this.snackbar.open(
+          "Oh não, algo aconteceu de errado!",
+          "Ok",
+          {
+            verticalPosition:'top',
+            horizontalPosition:'end',
+            duration:3000
+          }
+        
+    )
+  }})
+   }else{
+    //Irá alterar o usuário do banco de dados
+  
+   } 
+  
+  }
+  
+  
+  
+  onCancelar(){
+  this.material.reset();
+  this.material.disable();
+  }
+  
+  //Função para buscar as informações e usuários
+  
+  relatorio:any[] = [];
+  
+  buscaMaterial(){
+    this.materialService.getMaterial().subscribe({
+      next:(resposta)=>{
+        console.log(resposta);
+        this.relatorio = resposta.body;
+      },
+      error:(erro)=>{
+        console.log(erro);
+      }
+  
+    })
+  }
+  
+  
+  }
+  
+
+
